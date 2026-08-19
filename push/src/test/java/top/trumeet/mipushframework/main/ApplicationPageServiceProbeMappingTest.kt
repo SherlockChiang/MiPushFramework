e4edb59d86e8261c557bc9050b5fab0f88d9c98f
SherlockChiang <sherlockchiang@gmail.com>
2026-08-20 07:56:58 +0800

    test: initialize logging for service probe mapping

--- push/src/test/java/top/trumeet/mipushframework/main/ApplicationPageServiceProbeMappingTest.kt
+++ push/src/test/java/top/trumeet/mipushframework/main/ApplicationPageServiceProbeMappingTest.kt
@@ -1,12 +1,19 @@
 package top.trumeet.mipushframework.main
 
+import com.elvishew.xlog.XLog
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import top.trumeet.mipush.provider.entities.RegisteredApplication.ServiceProbeState
 import top.trumeet.mipushframework.main.subpage.ApplicationPageOperation
 import top.trumeet.mipushframework.utils.MiPushManifestChecker.ServiceCheckResult
 
 class ApplicationPageServiceProbeMappingTest {
+    @Before
+    fun initializeLogging() {
+        XLog.init()
+    }
+
     @Test
     fun checkerResultMapsToUiProbeState() {
         assertEquals(
